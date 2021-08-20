09417d5354670d420145a952eaffb4d405428555
Robert Baldwin <rob@wonolo.com>
2021-08-20 11:17:08 -0400

    add Javascript key value store test

---
+++ javascript/memory_key_value_store/memoryKeyValueStore.js
@@ -0,0 +1,18 @@
+class MemoryKeyValueStore {
+  constructor () {
+  }
+
+  get () {
+    return null
+  }
+
+  set () {
+    return null
+  }
+
+  keys () {
+    return null
+  }
+}
+
+module.exports = MemoryKeyValueStore
---
+++ javascript/memory_key_value_store/test/memoryKeyValueStoreTest.js
@@ -0,0 +1,50 @@
+const MemoryKeyValueStore = require('../memoryKeyValueStore.js')
+const expect = require('chai').expect
+
+describe('MemoryKeyValueStore', function () {
+  beforeEach(function createMkvs () {
+    this.currentTest.mkvs = new MemoryKeyValueStore()
+  })
+  describe('#get()', function () {
+    context('with a matching key', function () {
+      it('gets the value of key', function () {
+      })
+    })
+
+    context('without a matching key', function () {
+      it('returns the special value null', function () {
+      })
+    })
+
+    context('with a non-string value', function () {
+      it('returns an error', function () {
+      })
+    })
+  })
+  describe('#set', function () {
+    context('when the key does not hold a value', function () {
+      it('sets the key to hold the string value', function () {
+      })
+      it("it returns the string 'OK'", function () {
+      })
+    })
+
+    context('when the key already holds a value', function () {
+      it('sets the key to hold the string value', function () {
+      })
+      it("returns the string 'OK'", function () {
+      })
+    })
+  })
+
+  describe('#keys()', function () {
+    context('when no regex pattern is passed', function () {
+      it('returns all keys', function () {
+      })
+    })
+    context('when a regex pattern is passed', function () {
+      it('returns all keys matching regex pattern', function () {
+      })
+    })
+  })
+})
